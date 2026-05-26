6c6d0ae4269d4da30ce6a2d0172e470c47ca01d9
Charles Francoise <charles.francoise@circleci.com>
2026-05-26 15:15:31 +0200

    refactor: co-locate deriveSyllabusDraft with the syllabus route

--- lib/syllabus-chat/index.ts
+++ lib/syllabus-chat/index.ts
@@ -1,6 +1,5 @@
 export { bootstrapJourney } from './bootstrap';
 export type { BootstrapJourneyInput, BootstrapResult } from './bootstrap';
-export { deriveSyllabusDraftsFromMessages } from './derive-syllabus-draft';
 export { composeSyllabusSystemPrompt } from './prompts';
 export type { ComposeSyllabusSystemPromptParams } from './prompts';
 export { updateSyllabusDraftTool } from './tool';
